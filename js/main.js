edc276116154f7555dfacd45db688d5d2f3c022f
tuuy3n <uyenttt20411@st.uel.edu.vn>
2022-12-15 11:59:27 +0700

    update new header, footer

--- js/main.js
+++ js/main.js
@@ -14,7 +14,14 @@
     
     // Initiate the wowjs
     new WOW().init();
-    
+     // Sticky Navbar
+     $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top');
+        } else {
+            $('.nav-bar').removeClass('sticky-top');
+        }
+    });
     
     // Dropdown on mouse hover
     const $dropdown = $(".dropdown");
